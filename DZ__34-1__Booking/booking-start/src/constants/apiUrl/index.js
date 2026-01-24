7d3a8352f4a58e56963fe5381125adf59b0644c8
SerhiiPopov-Odessa <ukrpowta@gmail.com>
2026-01-24 13:48:55 +0100

    + .env

--- DZ__34-1__Booking/booking-start/src/constants/apiUrl/index.js
+++ DZ__34-1__Booking/booking-start/src/constants/apiUrl/index.js
@@ -1,4 +1,7 @@
+//export const BASE_URL ="http://localhost:3000/"
 //export const BASE_URL = "/api";
-export const BASE_URL = import.meta.env.DEV
+/* export const BASE_URL = import.meta.env.DEV
   ? "http://localhost:3000"
   : "/api";
+ */
+export const BASE_URL = import.meta.env.VITE_API_URL;
