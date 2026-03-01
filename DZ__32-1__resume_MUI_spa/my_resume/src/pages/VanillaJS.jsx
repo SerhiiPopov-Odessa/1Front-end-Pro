0004de52cc1fa9d274be3b58d6d016323353db74
SerhiiPopov-Odessa <ukrpowta@gmail.com>
2026-03-01 15:38:52 +0100

    + name projeck

--- DZ__32-1__resume_MUI_spa/my_resume/src/pages/VanillaJS.jsx
+++ DZ__32-1__resume_MUI_spa/my_resume/src/pages/VanillaJS.jsx
@@ -8,7 +8,7 @@ const VanilaJS = () => {
       <div className="flex flex-col items-center justify-start p-4">
         <h1 className="text-xl text-fuchsia-700/75 text-center mb-4 font-semibold bg-amber-300 rounded-2xl px-8">Vanilla JS</h1>
         
-        <p className="text-xl text-amber-700/90 font-semibold text-center bg-blue-400/50 w-fit mx-auto px-4 rounded-2xl mb-2">HTML CSS VanillaJS Fetch a JSON File API https://api.openweathermap.org</p>
+        <p className="text-xl text-amber-700/90 font-semibold text-center bg-blue-400/50 w-fit mx-auto px-4 rounded-2xl mb-2">Weather (HTML CSS VanillaJS Fetch a JSON File API https://api.openweathermap.org)</p>
         
         <div className="mb-6 ">
           <a href="https://weather-mocha-zeta-35.vercel.app/" target="_blank" rel="noopener noreferrer">
@@ -16,7 +16,7 @@ const VanilaJS = () => {
           </a>
         </div>
 
-        <p className="text-xl text-amber-700/90 font-semibold text-center bg-lime-600/50 w-fit mx-auto px-4 rounded-2xl mb-1">HTML VanillaJS + CSS</p>
+        <p className="text-xl text-amber-700/90 font-semibold text-center bg-lime-600/50 w-fit mx-auto px-4 rounded-2xl mb-1">Slider (HTML VanillaJS + CSS)</p>
         <div className="">
           <a href="https://slider-beta-nine.vercel.app/" target="_blank" rel="noopener noreferrer">
             <img className=" rounded-3xl " src={sliderPNG} alt="sliderPNG" />
