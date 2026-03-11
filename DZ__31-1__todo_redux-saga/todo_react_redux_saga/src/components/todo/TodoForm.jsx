c3d82ad257cce37075bf4abfe77d164c0e977999
SerhiiPopov-Odessa <ukrpowta@gmail.com>
2026-03-11 12:51:39 +0100

    +

--- DZ__31-1__todo_redux-saga/todo_react_redux_saga/src/components/todo/TodoForm.jsx
+++ DZ__31-1__todo_redux-saga/todo_react_redux_saga/src/components/todo/TodoForm.jsx
@@ -43,7 +43,7 @@ function TodoForm() {
       >
         {({ isSubmitting, touched, errors }) => (
           <Form className=" xl:w-[50%] flex flex-wrap items-center gap-2 sm:gap-4" >
-            <div className={clsx("flex-1 min-w-[200px]",styles.inputBlock)}>
+            <div className={clsx("flex-1 min-w-[200px] sm:min-w-[500px]",styles.inputBlock)}>
               <Field type="text" name="todoInput" className="w-full border rounded px-3 py-2"/> 
             </div>
             <button type="submit" disabled={isSubmitting} className="sm:px-6 sm:py-2 bg-blue-500 text-lg sm:text-2xl text-white rounded shrink-0 flex items-center justify-center">
