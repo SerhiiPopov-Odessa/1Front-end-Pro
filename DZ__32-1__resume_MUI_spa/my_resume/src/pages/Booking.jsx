8e8b1d5938ec432b76ad6908fe91566f21660b35
SerhiiPopov-Odessa <ukrpowta@gmail.com>
2026-03-13 18:08:04 +0100

    + skill

--- DZ__32-1__resume_MUI_spa/my_resume/src/pages/Booking.jsx
+++ DZ__32-1__resume_MUI_spa/my_resume/src/pages/Booking.jsx
@@ -6,7 +6,7 @@ const Booking = () => {
     <>
       <div className="flex flex-col items-center justify-start p-4">
         <h1 className="text-xl text-fuchsia-700/75 text-center mb-4 font-semibold bg-amber-300 rounded-2xl px-8">Booking</h1>
-        <p className="text-xl text-amber-700/90 font-semibold text-center bg-lime-600/50 w-fit mx-auto px-4 rounded-2xl  mb-4">React MUI Express.js JSON Server </p>
+        <p className="text-xl text-amber-700/90 font-semibold text-center bg-lime-600/50 w-fit mx-auto px-4 rounded-2xl  mb-4">React MUI TanStackQuery Express.js JSON Server </p>
         <div>
           <a href="https://dz34-1booking.vercel.app/" target="_blank" rel="noopener noreferrer">
             <img  src={bookingPNG} alt="bookingPNG" />
